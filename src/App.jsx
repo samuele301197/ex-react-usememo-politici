ff00e7177e2168f367c8cd4c1d41f6ebcc11f1be
samuele301197 <parisisamu32@gmail.com>
2025-09-18 13:31:53 +0200

    feat: milestone-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useMemo, useState } from "react";
 import politicians from "./db.js";
 
 function App() {
   const [politics, setPolitics] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     setPolitics(politicians);
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+    return politics.filter((politic) => {
+      const isInName = politic.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const isInDesc = politic.description
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return isInName || isInDesc;
+    });
+  }, [politics, search]);
+
   return (
     <>
       <h1>Lista Politici</h1>
-      {politics.map((p) => (
+      <input
+        type="text"
+        placeholder="cerca un politico"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {filteredPoliticians.map((p) => (
         <div>
           <h3>{p.name}</h3>
           <h4>{p.role}</h4>
